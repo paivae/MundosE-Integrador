d5243cd50ee54799670916e4fbc41d1d5562adca
paivae <18567047+paivae@users.noreply.github.com>
2024-11-20 18:38:16 -0300

    test con falla

--- web/app.js
+++ web/app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 80
 
+app.get('/ping', (req, res) => {
+  res.send('pong')
+})
+
+
 app.get('/', (req, res) => {
   res.send('Funcionaaa!!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
